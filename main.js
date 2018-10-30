6b74824dde877666a5306c1443fa0d899e755a0c
Zaira Gonzalez <zgonzalez@nyc.yearup.org>
2018-10-30 15:45:23 -0400

    First Java commit

---
+++ main.js
@@ -0,0 +1,23 @@
+var prices = [33, 26, 99, 120, 12, 45];
+var sum = 0;
+
+
+for (var i=0; 1 < prices.length; i++){
+  sum += i;
+}
+  console.log("The sum is:", sum);
+
+
+ /*seperation*/
+
+counter = 99;
+
+  while (counter > 1){
+  console.log(counter + " crows on the wall. " + counter + " crows.");
+  console.log("1 fell down and became a wight.");
+  counter--;
+  console.log(counter + " on the wall." );
+  console.log();
+  }
+
+  console.log("1 crow on the wall. 1 single crow. \n It fell down and became a wight.\n There's no one left to defend Westeros now. ")
